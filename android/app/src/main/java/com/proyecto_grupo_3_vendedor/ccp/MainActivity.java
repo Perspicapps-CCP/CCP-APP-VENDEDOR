13336e60e249a6e05e0ad2c89f468358fc36b08d
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-04-19 16:51:14 -0500

    feat: refactor MainActivity and update Google Maps API key in environment files

---
+++ android/app/src/main/java/com/proyecto_grupo_3_vendedor/ccp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.proyecto_grupo_3_vendedor.ccp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://192.168.1.23:3000/mock/ccp',
   apiUrlCCP: 'http://34.8.227.72',
-  googleMapsApiKey: 'AIzaSyCGV_lWE5TUh0IgavgTt9ZuPfhFc-lNGD0',
+  googleMapsApiKey: 'AIzaSyClja14TPGyVx4PfJOWGZWaYm9Valb899E',
 };
