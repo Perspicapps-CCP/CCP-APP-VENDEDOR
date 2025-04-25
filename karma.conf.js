4ff62ec6865997a86a785d936f518f85f2c868b9
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-04-24 19:02:57 -0500

    refactor: clean up karma configuration and enhance RutasService tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,35 +10,28 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app'),
-      subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
-      ]
+        { type: 'lcov' }, // para herramientas de CI/CD
+        { type: 'text-summary' }, // útil para ver resultados en la consola
+      ],
+      dir: require('path').join(__dirname, './coverage/ccp-web'),
+      subdir: '.',
+      check: {
+        global: {
+          statements: 80,
+        },
+      },
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['Chrome', 'ChromeHeadless'], // añadido ChromeHeadless para CI
+    singleRun: true,
+    restartOnFileChange: true,
   });
 };
