fde98cc811e6fb68752ac4f480c0080759c229b7
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-05-18 11:30:29 -0500

    Update Capacitor keyboard plugin to version ^7.0.0 and configure keyboard settings in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,4 +1,5 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.proyecto_grupo_3_vendedor.ccp',
@@ -15,6 +16,11 @@ const config: CapacitorConfig = {
       backgroundColor: '#3880ff', // Color principal de tu app
       style: 'light',
       overlaysWebView: false,
+    },
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true,
     },
   },
   android: {
